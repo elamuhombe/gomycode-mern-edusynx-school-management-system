b1aacb2cadd5813c99b71d76a803c50cf8453c2b
Elaine Muhombe <emuhombe@gmail.com>
2024-03-10 11:30:31 +0300

    Intialize project and add project files

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+import { Home } from "./pages";
+
+export default function App() {
+  return <div className="bg-red-500 text-white">
+    <Home />
+  </div>;
+}
---
+++ client/src/components/shared/PageWrapper.tsx
@@ -0,0 +1,8 @@
+interface Props{
+    children: React.ReactNode;
+}
+export default function PageWrapper({children}: Props) {
+  return (
+    <div>PageWrapper</div>
+  )
+}
