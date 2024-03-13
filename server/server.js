f43cde750ee1e96256b5ba8f65c5712e533c6d44
Elaine Muhombe <emuhombe@gmail.com>
2024-03-13 17:52:21 +0300

    chore: Add initial setup: Import required modules and create an instance of the Express server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+// Import required modules
+const express = require('express');
+const mongoose = require('mongoose');
+require ('dotenv').config;
+
+// Create an instance of the express server
+const app = express();
+const PORT = 5000;
+
+
+
+
