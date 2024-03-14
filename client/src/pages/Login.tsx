141e2dd3d1ff78efedd658b51edfb2fb2f5068d1
Elaine Muhombe <emuhombe@gmail.com>
2024-03-14 23:18:42 +0300

    feat: Add login form component with Tailwind CSS styling

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -11,10 +11,40 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center p-4">
+    <div className="flex flex-col items-center min-h-screen justify-center p-4">
       <header>
        <h2 className='mt-8'>edusynx school management system</h2>
       </header>
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          className="shadow appearance-none border rounded w-60 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
+        />
+        </div>
+        <div>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          className="shadow appearance-none border rounded w-60 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
+        />
+        </div>
+        
+        <div>
+        <button type="submit" className="w-60 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          Login
+        </button>
+        </div>
+       <p className='mt-4'>Forgot Your Password?<a href="#">Reset</a></p>
+        
+      </form>
       
     </div>
   );
