458abe4b36328e07571b7b7b87609018f2787402
Elaine Muhombe <emuhombe@gmail.com>
2024-03-14 22:57:00 +0300

    feat: Add header tag with title

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,23 @@
 
-export default function Home() {
-  return (
-    <div>Home</div>
-  )
-}
+import React, { useState } from 'react';
 
+const Login: React.FC = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center p-4">
+      <header>
+       <h2 className='mt-8'>edusynx school management system</h2>
+      </header>
+      
+    </div>
+  );
+};
+
+export default Login;
