0e9a62c42cb5e39f0a6d4acd601bba9e121a8c5d
Elaine Muhombe <emuhombe@gmail.com>
2024-03-18 22:29:36 +0300

    feat: Add copyright information and navigation links

--- client/src/components/shared/Footer.tsx
+++ client/src/components/shared/Footer.tsx
@@ -2,6 +2,16 @@
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <footer>
+        <p>© 2024 Edusynx School Management System. All rights reserved.</p>
+        <nav>
+          <ul>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms of Service</a></li>
+          </ul>
+        </nav>
+        </footer>
+    </div>
   )
 }
