8aea1f8886325cc7c312d18b0abf1f1e8180ffdd
Elaine Muhombe <emuhombe@gmail.com>
2024-03-18 22:25:59 +0300

    feat: Add header and navigation links and style with tailwind css

--- client/src/components/shared/Header.tsx
+++ client/src/components/shared/Header.tsx
@@ -2,6 +2,18 @@
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div>
+    <header className="flex justify-between items-center">
+      <h1 className="text-4 font-bold text-gray-800 pb-4">Edusynx School Management System</h1>
+      <nav className="flex">
+        <ul className="flex space-x-4">
+          <li><a href="#" className="text-gray-800 hover:text-gray-600">Home</a></li>
+          <li><a href="#" className="text-gray-800 hover:text-gray-600">Contact</a></li>
+          <li><a href="#" className="text-gray-800 hover:text-gray-600">Login</a></li>
+          <li><a href="#" className="text-gray-800 hover:text-gray-600">Register</a></li>
+        </ul>
+      </nav>
+    </header>
+  </div>  
   )
 }
