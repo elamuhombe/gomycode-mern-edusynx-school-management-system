da1d6900c5d3901992242c0df719081206df6b4c
Elaine Muhombe <emuhombe@gmail.com>
2024-03-19 22:10:51 +0300

    chore: Add comments to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
-import { Footer, Header, Main } from './components/index'
+// Importing necessary components from the components folder
+import { Footer, Header, Main } from "./components/index";
 
-
+// Defining the App component
 export default function App() {
   return (
+    // Wrapping the components in a div with a margin of 4
     <div className="m-4">
-    <Header/>
-    <Main />
-    <Footer />
+      {/* Rendering the Header component */}
+      <Header />
+      {/* Rendering the Main component */}
+      <Main />
+      {/* Rendering the Footer component */}
+      <Footer />
     </div>
   );
 }
