66326dc0c10ab34cb0aa82e2eefba1c5bbb5c681
Elaine Muhombe <emuhombe@gmail.com>
2024-03-19 22:13:07 +0300

    chore: Modify hero section heading

--- client/src/components/shared/Main.tsx
+++ client/src/components/shared/Main.tsx
@@ -1,60 +1,109 @@
-
-
 export default function Main() {
   return (
-   <div className="flex-row">
-   
-    {/* Hero Section */}
-    <section className="flex">
-      <div className="flex-1">
-      <h1 className="text-2xl font-lato font-bold mt-8">Welcome to Edusynx School Management System, where education meets innovation!</h1>
-    <p className="mt-4">Are you tired of the old-fashioned administrative hassles that come with managing a school? Look no further! Our cutting-edge platform is designed to streamline every aspect of school management, from student enrollment to academic performance tracking.</p>
-      </div>
-      <div className="flex-1 flex justify-center">
-      <img src="/images/school-system.jpg" alt="school system" />
-
-
-      </div>
+    <div className="flex flex-col px-6">
+      {/* Hero Section */}
+      <section className="flex">
+        <div className="flex-1">
+          <h1 className="text-2xl font-lato font-bold mt-8">Edusynx School Management System, where education meets innovation!</h1>
+          <p className="mt-4">Are you tired of the old-fashioned administrative hassles that come with managing a school? Look no further! Our cutting-edge platform is designed to streamline every aspect of school management, from student enrollment to academic performance tracking.</p>
+        </div>
+        <div className="flex-1 flex justify-center">
+          <img src="/images/school-system.jpg" alt="school system" />
+        </div>
+      </section>
     
-    </section>
+      {/* Main Features Section */}
+      <section className="flex">
+      <h2 className="flex mt-20 flex-row">Main Feature</h2>
+      <div className="flex mt-8 justify-center gap-4 flex-wrap">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <img className="w-40 h-40 mx-auto" src="/images/student.svg" alt="Student image" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Students Module</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget libero at quam fermentum iaculis ut nec eros.
+            </p>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag3</span>
+          </div>
+        </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <img className="w-40 h-40 mx-auto" src="/images/teacher.svg" alt="Teacher image" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Teachers Module</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget libero at quam fermentum iaculis ut nec eros.
+            </p>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag3</span>
+          </div>
+        </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <img className="w-40 h-40 mx-auto" src="/images/accounts.svg" alt="Accounts image" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Accounts Module</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget libero at quam fermentum iaculis ut nec eros.
+            </p>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag3</span>
+          </div>
+        </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <img className="w-40 h-40 mx-auto" src="/images/admin.svg" alt="Admin image" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Admin Module</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget libero at quam fermentum iaculis ut nec eros.
+            </p>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag3</span>
+          </div>
+        </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <img className="w-40 h-40 mx-auto" src="/images/report.svg" alt="Report image" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Reports Module</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget libero at quam fermentum iaculis ut nec eros.
+            </p>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag3</span>
+          </div>
+        </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <img className="w-40 h-40 mx-auto" src="/images/parents.svg" alt="Card image" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Parents Module</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget libero at quam fermentum iaculis ut nec eros.
+            </p>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag3</span>
+          </div>
+        </div>
 
-    {/* Main Features Section */}
-    <section className="flex mt-8">
-       <h2 className="text-2xl  text-center font-semi-bold">Main Features</h2>
-     
-
-      <div>
-      <div>
-      <h5>Student</h5>
-     </div>
-     <div>
-      <h5>Teachers</h5>
-     </div>
-     <div>
-      <h5>Attendance</h5>
-     </div>
-     <div>
-      <h5>Accounts</h5>
-     </div>
-     <div>
-      <h5>Parents</h5>
-     </div>
-     <div>
-      <h5>Reports</h5>
-     </div>
-     <div>
-      <h5>Admin</h5>
-     </div>
-     <div>
-      <h5>Enrollment</h5>
-     </div>
       </div>
-     
-
-
-    </section>
-    
-      
-   </div>
-  )
+        
+      </section>
+    </div>
+  );
 }
