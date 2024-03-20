1af333af8ed83dedbda35d9a6adebdd81662a732
Elaine Muhombe <emuhombe@gmail.com>
2024-03-20 19:50:54 +0300

    Added createRoot from react-dom/client for React 18 compatibility

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from 'react';
+import { createRoot } from 'react-dom/client'; // Import createRoot from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error("Root element not found in the document");
+}
+
+const root = createRoot(rootElement); // Use createRoot instead of ReactDOM.createRoot
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
+
+
+
