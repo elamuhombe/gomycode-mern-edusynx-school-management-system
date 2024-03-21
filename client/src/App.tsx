7eac5f39e04d7c8238e872c52000eacd88d4fabf
Elaine Muhombe <emuhombe@gmail.com>
2024-03-21 19:34:42 +0300

    chore: Import Header, Main and Footer components from index.ts file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 // Importing necessary components from the components folder
-import HomePageWrapper from "./components/shared/templates/Home/HomePageWrapper";
+import {Header, Main, Footer} from './pages/index';
 
 
 // Defining the App component
@@ -7,7 +7,9 @@ export default function App() {
   return (
     // Wrapping the components in a div with a margin of 4
     <div className="m-4">
-      <HomePageWrapper />
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
