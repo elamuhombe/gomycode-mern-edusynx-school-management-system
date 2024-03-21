5b4bd893fef66360658959e8c284bbfcd233cb61
Elaine Muhombe <emuhombe@gmail.com>
2024-03-21 19:36:13 +0300

    Delete index.ts file in components directory

--- client/src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export {default as Footer} from './shared/templates/Home/Footer/Footer';
-export {default as Header} from './shared/templates/Home/Header/Navbar';
-export {default as Main} from './shared/templates/Home/Main/Main';
