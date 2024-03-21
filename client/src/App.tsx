a47ca20c39862e8bcfc1edd804f065c63c359b72
Elaine Muhombe <emuhombe@gmail.com>
2024-03-21 18:05:26 +0300

    chore: Modify Home component to HomePageWrapper

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,13 @@
 // Importing necessary components from the components folder
-import { Footer, Header, Main } from "./components/index";
+import HomePageWrapper from "./components/shared/templates/Home/HomePageWrapper";
+
 
 // Defining the App component
 export default function App() {
   return (
     // Wrapping the components in a div with a margin of 4
     <div className="m-4">
-      {/* Rendering the Header component */}
-      <Header />
-      {/* Rendering the Main component */}
-      <Main />
-      {/* Rendering the Footer component */}
-      <Footer />
+      <HomePageWrapper />
     </div>
   );
 }
