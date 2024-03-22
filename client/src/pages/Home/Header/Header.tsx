000f960b79d850216887fd249165a12a5ef323fd
Elaine Muhombe <emuhombe@gmail.com>
2024-03-22 18:57:02 +0300

    chore: Add positioning to Logo and Navbar components

--- client/src/pages/Home/Header/Header.tsx
+++ client/src/pages/Home/Header/Header.tsx
@@ -1,16 +1,23 @@
-
 import Logo from "./Logo";
 import Navbar from "./Navbar";
 
 const Header: React.FC = () => {
   return (
     <div className="flex items-center justify-between p-2">
-      <Navbar />
-      <Logo />
-       
+      {/* Navbar appears on the right for large screens */}
+      <div className="lg:order-2">
+        <Navbar />
+      </div>
+      {/* Logo appears on the left for small screens */}
+      <div className="order-1 lg:order-1">
+        <Logo />
+      </div>
+      {/* Hide one Navbar for small screens */}
+      <div className="lg:hidden hidden">
+        <Navbar />
+      </div>
     </div>
   );
 }
 
 export default Header;
-
