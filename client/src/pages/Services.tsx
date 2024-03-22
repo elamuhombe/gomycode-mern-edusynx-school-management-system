8f56d82615e80bf84b734933f2990fe320e28051
Elaine Muhombe <emuhombe@gmail.com>
2024-03-22 19:53:25 +0300

    chore: Add heading

--- client/src/pages/Services.tsx
+++ client/src/pages/Services.tsx
@@ -1,2 +1,13 @@
+import React from 'react';
 
-==
+const Services: React.FC = () => {
+  return (
+    <div>
+      <h1>Services</h1>
+      {/* Add your service-related content here */}
+    </div>
+  );
+};
+
+export default Services;
+
