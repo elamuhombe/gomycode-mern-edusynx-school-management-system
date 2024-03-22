87168f807406fa0725ad59b51d0ad825fe5f74c4
Elaine Muhombe <emuhombe@gmail.com>
2024-03-22 20:03:25 +0300

    feature: Add Documentation.tsx and FAQ.tsx components in support directory

---
+++ client/src/pages/support/Documentation.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Documentation: React.FC = () => {
+  return (
+    <div>
+      <h1>Document</h1>
+      <p>Welcome to our document section!</p>
+      {/* Add your document content here */}
+    </div>
+  );
+};
+
+export default Documentation;
---
+++ client/src/pages/support/FAQ.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FAQ: React.FC = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default FAQ;
