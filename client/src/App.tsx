e0ce80979854c4f1f1a9fe801f89cb93cf9d798e
Elaine Muhombe <emuhombe@gmail.com>
2024-03-23 20:38:09 +0300

    chore: Add routes for Login, Register, ContactUs, Services, and Home pages in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,30 @@
-// Importing necessary components from the components folder
-import {Header, Main, Footer} from './pages/index';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home';
+
+import Services from './pages/Services';
+import ContactUs from './pages/ContactUs';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 
-// Defining the App component
-export default function App() {
+const App: React.FC = () => {
   return (
-    // Wrapping the components in a div with a margin of 4
-    <div className="m-4 font-sans">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <Router>
+      <div className="m-4 font-sans">
+      
+        <Routes>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
+
+export default App;
