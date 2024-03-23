6812df9908c8eb44538ec64717c7d57d47fe1e81
Elaine Muhombe <emuhombe@gmail.com>
2024-03-23 20:57:13 +0300

    chore: Delete Footer component in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
 
 import Services from './pages/Services';
@@ -21,7 +20,7 @@ const App: React.FC = () => {
           <Route path="/Services" element={<Services />} />
           <Route path="/" element={<Home />} />
         </Routes>
-        <Footer />
+      
       </div>
     </Router>
   );
