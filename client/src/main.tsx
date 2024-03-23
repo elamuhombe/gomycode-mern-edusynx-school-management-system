1b432aa29f4fe33a2a10a9947520bea9f9702b3b
Elaine Muhombe <emuhombe@gmail.com>
2024-03-23 20:54:17 +0300

    chore: Import tailwind css styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,24 +1,8 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Import createRoot from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import 'tailwindcss/tailwind.css'; // Import Tailwind CSS styles
 
-const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  throw new Error("Root element not found in the document");
-}
-
-const root = createRoot(rootElement); // Use createRoot instead of ReactDOM.createRoot
-
-root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
-);
-
-
-
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
