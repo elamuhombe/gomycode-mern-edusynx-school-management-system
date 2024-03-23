85bd2c383c68864046d9b71dc6c246646a450651
Elaine Muhombe <emuhombe@gmail.com>
2024-03-23 20:46:42 +0300

    chore: Add React import

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+
+
+
+import React from 'react';
+import { Footer, Header, Main } from '../components';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+     <Header />
+     <Main />
+     <Footer />
+    </div>
+  );
+}
+
+export default Home;
