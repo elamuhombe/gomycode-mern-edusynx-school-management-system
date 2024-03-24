f2c6f5271f6734146dd3201502ac38dc158fc601
Elaine Muhombe <emuhombe@gmail.com>
2024-03-24 14:17:11 +0300

    chore: Add contact form component

--- client/src/components/Main/Contacts.tsx
+++ client/src/components/Main/Contacts.tsx
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { TextField, Button } from '@mui/material';
 
 
 const Contacts: React.FC = () => {
   
 
   return (
-    <div>
-      <h3>Contacts</h3>
+    <div className="flex">
       {/* Form to add new contact */}
-      <form>
-        
+      <form className='flex flex-col flex-1 gap-4'>
+        <h4>Get In Touch</h4>
+        <p>Enim tempor eget pharetra facilisis sed maecenas adipiscing. Eu leo molestie vel, ornare non id blandit netus.</p>
+      <div>
+      <div className='flex flex-1 flex-col w-80'>
+      <TextField id="standard-basic" label="Names" variant="standard" />
+      <TextField id="standard-basic" label="Phone Number" variant="standard" />
+      <TextField id="standard-basic" label="Email Address" variant="standard" />
+    </div>
+    <div>
+      <Button variant="contained" color="primary">
+        Submit
+      </Button>
+    </div>
+    
+        </div>
+      
       </form>
+      <div className='flex flex-1'>
+    <p>Google Maps</p>
+    </div>
     </div>
   );
 };
