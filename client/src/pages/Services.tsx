5526a58f8a9c668ca8704efb3b1a83983b437b68
Elaine Muhombe <emuhombe@gmail.com>
2024-03-24 19:04:05 +0300

    chore: Add Header and Footer

--- client/src/pages/Services.tsx
+++ client/src/pages/Services.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Footer, Header } from '../components';
 
 const Services: React.FC = () => {
   return (
     <div>
-      <h1>Services</h1>
+      <Header />
+      <Footer />
      </div>
   );
 };
