fd612a2b1e53ddd5201811c84bc408b16401b820
Elaine Muhombe <emuhombe@gmail.com>
2024-03-24 13:46:11 +0300

    feat: Create Contacts component and insert form

---
+++ client/src/components/Main/Contacts.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+
+const Contacts: React.FC = () => {
+  
+
+  return (
+    <div>
+      <h3>Contacts</h3>
+      {/* Form to add new contact */}
+      <form>
+        
+      </form>
+    </div>
+  );
+};
+
+export default Contacts;
