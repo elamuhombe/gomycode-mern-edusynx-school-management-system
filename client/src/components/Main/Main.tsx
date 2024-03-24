435f3c6b4f5873ae9b03ca136387e1fadce5a71b
Elaine Muhombe <emuhombe@gmail.com>
2024-03-24 13:44:35 +0300

    feat: Add Contacts component

--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./Hero";
 import Features from "./Features";
+import Contacts from "./Contacts";
 export default function Main() {
   return (
     <div className="flex flex-col px-6 min-h-[600px]">
       <Hero />
       <Features />
+      <Contacts />
     </div>
   );
 }
