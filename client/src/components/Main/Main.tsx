d5079676aea05ea97c9002df4ec880b122138da8
Elaine Muhombe <emuhombe@gmail.com>
2024-03-25 22:20:27 +0300

    chore: Mute Features component

--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
-import Features from "./Features";
+// import Features from "./Features";
 import Contacts from "./Contacts";
 export default function Main() {
   return (
     <div className="flex flex-col px-6 min-h-[600px]">
       <Hero />
-      <Features />
+      {/* <Features /> */}
       <Contacts />
     </div>
   );
