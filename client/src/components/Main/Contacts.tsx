2bf4c19ff070476036975f8ec6c26793d1ca95c6
Elaine Muhombe <emuhombe@gmail.com>
2024-03-25 22:09:17 +0300

    chore: Modify the paragraph text for contact form instructions

--- client/src/components/Main/Contacts.tsx
+++ client/src/components/Main/Contacts.tsx
@@ -1,34 +1,45 @@
-import React from 'react';
-import { TextField, Button } from '@mui/material';
-
+import React, { useState } from 'react';
+import { TextField } from '@mui/material';
 
 const Contacts: React.FC = () => {
-  
+  // State to store the message value
+  const [message, setMessage] = useState('');
+
+  // Function to handle changes in the message field
+  const handleMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  };
 
   return (
     <div className="flex">
       {/* Form to add new contact */}
       <form className='flex flex-col flex-1 gap-4'>
         <h4>Get In Touch</h4>
-        <p>Enim tempor eget pharetra facilisis sed maecenas adipiscing. Eu leo molestie vel, ornare non id blandit netus.</p>
-      <div>
-      <div className='flex flex-1 flex-col w-80'>
-      <TextField id="standard-basic" label="Names" variant="standard" />
-      <TextField id="standard-basic" label="Phone Number" variant="standard" />
-      <TextField id="standard-basic" label="Email Address" variant="standard" />
-    </div>
-    <div>
-      <Button variant="contained" color="primary">
-        Submit
-      </Button>
-    </div>
-    
+        <p>Please feel free to contact us if you have any questions, comments, or inquiries. We'd love to hear from you!</p>
+        <div className='flex flex-1 flex-col w-80'>
+          <TextField id="names" label="Names" variant="standard" />
+          <TextField id="phoneNumber" label="Phone Number" variant="standard" />
+          <TextField id="emailAddress" label="Email Address" variant="standard" />
+          {/* Text field for the message */}
+          <TextField
+            id="message"
+            label="Message"
+            variant="standard"
+            value={message}
+            onChange={handleMessageChange}
+            multiline
+            rows={4}
+          />
         </div>
-      
+        <div>
+          <button className="bg-red-400 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mt-4" >
+            Submit
+          </button>
+        </div>
       </form>
       <div className='flex flex-1'>
-    <p>Google Maps</p>
-    </div>
+        <p>Google Maps</p>
+      </div>
     </div>
   );
 };
