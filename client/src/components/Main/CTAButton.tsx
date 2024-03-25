f77b3af1495982840245c64b637053944616e84c
Elaine Muhombe <emuhombe@gmail.com>
2024-03-25 21:56:16 +0300

    feat: Add navigation functionality to CTA Button

--- client/src/components/Main/CTAButton.tsx
+++ client/src/components/Main/CTAButton.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-
+import { useCustomNavigation } from '../../hooks/useNavigation'; // Import the custom navigation hook
 
 interface CTAButtonProps {
   text: string;
-  onClick: () => void;
 }
 
-const CTAButton: React.FC<CTAButtonProps> = ({ text, onClick }) => {
+const CTAButton: React.FC<CTAButtonProps> = ({ text }) => {
+  const { goToRegisterPage } = useCustomNavigation(); // Call the hook to get the navigation function
+
+  // Click handler function to navigate to the register page
+  const handleClick = () => {
+    goToRegisterPage(); // Use the navigation function to navigate
+  };
+
   return (
- 
-        <button className="bg-red-400 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mt-4" onClick={onClick}>
+    <button 
+      className="bg-red-400 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mt-4" 
+      onClick={handleClick} // Attach the click handler
+    >
       {text}
     </button>
-   
-    
   );
 };
 
