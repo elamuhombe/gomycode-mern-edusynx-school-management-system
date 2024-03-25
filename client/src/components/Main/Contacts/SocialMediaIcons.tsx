9aeb62b2bfaecc4a915550c128bfad952cb1e950
Elaine Muhombe <emuhombe@gmail.com>
2024-03-25 22:42:41 +0300

    feat: Add Social Media Icons component

---
+++ client/src/components/Main/Contacts/SocialMediaIcons.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface SocialMediaIconsProps {
+  // Define props here if needed
+}
+
+const SocialMediaIcons: React.FC<SocialMediaIconsProps> = () => {
+  // Implement the component logic here
+  return (
+    <div className="social-media-icons">
+      {/* Add your social media icons here */}
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
