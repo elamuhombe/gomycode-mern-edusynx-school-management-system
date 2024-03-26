79a2d5ed1406035ca334a4939ace9bce376edc58
Elaine Muhombe <emuhombe@gmail.com>
2024-03-26 17:54:33 +0300

    refractor: Remove click event in CTAButton

--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "./Hero";
 // import Features from "./Features";
-import Contacts from "./Contacts";
+import Contacts from "./Contacts/Contacts";
 export default function Main() {
   return (
     <div className="flex flex-col px-6 min-h-[600px]">
