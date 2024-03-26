856ecc39d55aaac08db9a9185574691a2db63d95
Elaine Muhombe <emuhombe@gmail.com>
2024-03-26 17:59:43 +0300

    refactor: Remove onclick attribute in CTAButton

--- client/src/components/Main/Hero.tsx
+++ client/src/components/Main/Hero.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import CTAButton from './CTAButton';
 
+
 const Hero: React.FC = () => {
-  const handleClick = () => {
-    alert('Button clicked!');
-  };
+ 
   return (
     <div>
       {/* Hero Section */}
@@ -28,7 +27,7 @@ const Hero: React.FC = () => {
               <ul className="bullet h-4 md:h-8">Accurate Fee Management</ul><br></br>
             </div>
           </div>
-          <CTAButton text="Get Started" onClick={handleClick} />
+          <CTAButton text="Get Started"/>
         </div>
         <div className="flex-1 flex justify-center">
           <img src="/images/school-software.png" alt="School System" />
