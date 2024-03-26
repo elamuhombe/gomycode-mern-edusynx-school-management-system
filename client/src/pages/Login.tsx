dfc840bdbbf6ff090ffa7f78fda1809abbeefc8a
Elaine Muhombe <emuhombe@gmail.com>
2024-03-26 23:17:33 +0300

    chore: Refractor Login component into LoginForm and Login components

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,60 +1,24 @@
-import React, { useState } from 'react';
-import { Footer, Header } from '../components';
+// Login.tsx
+import React from 'react';
+import { Footer, Header } from '../components'; // Assuming you have Header and Footer components
+import LoginForm from '../components/Login/LoginForm';
 
 const Login: React.FC = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-
-  const handleLogin = (event: React.FormEvent) => {
-    event.preventDefault();
-    // Here you can implement your login logic
-    if (username === 'example' && password === 'password') {
-      console.log('Login successful!');
-      // Redirect or perform any other action upon successful login
-    } else {
-      setError('Invalid username or password');
-    }
+  const handleLoginFormSubmit = (username: string, password: string) => {
+    // Handle form submission here, e.g., call API, perform authentication, etc.
   };
 
   return (
-    <div>
+    <div className="flex flex-col justify-between h-screen">
       <Header />
-      <div className="flex justify-center items-center h-screen">
-      <div className="w-full max-w-xs">
-        <h2 className="text-center text-2xl mb-4">Login</h2>
-        {error && <div className="text-red-500 mb-4">{error}</div>}
-        <form onSubmit={handleLogin}>
-          <div className="mb-4">
-            <label htmlFor="username" className="block mb-2">Username:</label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-full border border-gray-400 p-2"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block mb-2">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full border border-gray-400 p-2"
-              required
-            />
-          </div>
-          <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Login</button>
-        </form>
+      <div className="flex justify-center items-center mt-16">
+        <div className="w-full max-w-xs">
+          <h2 className="text-center text-2xl mb-4">Login</h2>
+          <LoginForm onSubmit={handleLoginFormSubmit} />
+        </div>
       </div>
+      <Footer />
     </div>
-    <Footer />
-
-    </div>
-    
   );
 };
 
