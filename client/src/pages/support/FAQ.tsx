c2a3e0394649134dfe31e8ad6d8526a7a1c61457
Elaine Muhombe <emuhombe@gmail.com>
2024-03-27 19:15:31 +0300

    style: Center the content in FAQ component

--- client/src/pages/support/FAQ.tsx
+++ client/src/pages/support/FAQ.tsx
@@ -1,33 +1,29 @@
 import React from 'react';
-import { Footer, Header } from '../../components';
 
 const FAQ = () => {
   // FAQ data specific to a school management system
   const faqs = [
-    { question: 'How do I register my school?', answer: 'Click on Register Link and fill out the form, then submit by clicking the submit button.' },
+    { question: 'How do I enroll my child in classes?', answer: 'You can enroll your child by visiting the school office.' },
     { question: 'What are the school hours?', answer: 'The school hours are from 8:00 AM to 3:00 PM, Monday to Friday.' },
-    { question: 'How can I pay school fees online?', answer: 'You can pay school fees online through the school' },
+    { question: 'How can I pay school fees online?', answer: 'You can pay school fees online through the schools website or portal.' },
     { question: 'Who can I contact for technical support with the school portal?', answer: 'You can contact the IT department at it@schoolname.com.' },
     // Add more FAQ items as needed
   ];
 
   return (
-    <div>
-      <Header />
-      <div className="faq">
-      <h2>School Management System FAQs</h2>
-      <ul>
-        {faqs.map((faq, index) => (
-          <li key={index}>
-            <h3>{faq.question}</h3>
-            <p>{faq.answer}</p>
-          </li>
-        ))}
-      </ul>
+    <div className="faq flex justify-center items-center h-full">
+      <div className="max-w-md w-full">
+        <h2 className="text-2xl font-semibold mb-4">School Management System FAQs</h2>
+        <ul>
+          {faqs.map((faq, index) => (
+            <li key={index} className="mb-4">
+              <h3 className="text-lg font-medium mb-2">{faq.question}</h3>
+              <p className="text-gray-700">{faq.answer}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-          <Footer />
-    </div>
-    
   );
 };
 
