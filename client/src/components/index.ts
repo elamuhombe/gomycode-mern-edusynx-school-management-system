4ff994c11ba96d98b842743e83321834d8821275
Elaine Muhombe <emuhombe@gmail.com>
2024-03-27 21:36:58 +0300

    chore: Update ReadMe.md and moved files

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Header } from './Header/Header';
+export { default as Header } from './shared/Header/Header';
 export { default as Main } from './Main/Main';
-export { default as Footer } from './Footer/Footer';
+export { default as Footer } from './shared/Footer/Footer';
 
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,35 +1,69 @@
-import React from 'react';
-import { Footer, Header } from '../components';
-import { TextField } from '@mui/material';
-import CTAButton from '../components/Main/CTAButton';
+import React from "react";
+import { Footer, Header } from "../components";
+import { TextField } from "@mui/material";
+import CTAButton from "../components/shared/CTAButton";
 
 const Register: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Header />
-      <div className='flex flex-col items-center w-full max-w-md px-4 justify-center'> {/* Added justify-center class */}
+      <div className="flex flex-col items-center w-full max-w-md px-4 justify-center">
+        {" "}
+        {/* Added justify-center class */}
         {/* Form to Register */}
-        <form className='flex flex-col w-full gap-4'>
-          <h3 className='text-xl font-600'>Register</h3>
+        <form className="flex flex-col w-full gap-4">
+          <h3 className="text-xl font-600">Register</h3>
           <p>Please fill out the registration form below.</p>
-          <div className='flex flex-col w-full gap-4'>
+          <div className="flex flex-col w-full gap-4">
             <div className="w-full">
-              <TextField id="schoolName" label="School Name" variant="standard" />
+              <TextField
+                id="schoolName"
+                label="School Name"
+                variant="standard"
+              />
             </div>
             <div className="w-full">
-              <TextField id="phoneNumber" label="Phone Number" variant="standard" />
+              <TextField
+                id="phoneNumber"
+                label="Phone Number"
+                variant="standard"
+              />
             </div>
             <div className="w-full">
-              <TextField id="emailAddress" label="Email Address" variant="standard" />
+              <TextField
+                id="emailAddress"
+                label="Email Address"
+                variant="standard"
+              />
             </div>
           </div>
-          <div className='flex flex-col md:flex-row w-full gap-4'>
-            <TextField id="postalAddress" label="Postal  Address" variant="standard" className='md:w-1/2' />
-            <TextField id="postalCode" label="Postal Code" variant="standard" className='md:w-1/4' />
+          <div className="flex flex-col md:flex-row w-full gap-4">
+            <TextField
+              id="postalAddress"
+              label="Postal  Address"
+              variant="standard"
+              className="md:w-1/2"
+            />
+            <TextField
+              id="postalCode"
+              label="Postal Code"
+              variant="standard"
+              className="md:w-1/4"
+            />
           </div>
-          <div className='flex flex-col md:flex-row w-full gap-4'>
-            <TextField id="city" label="City" variant="standard" className='md:w-1/2' />
-            <TextField id="country" label="Country" variant="standard" className='md:w-1/2' />
+          <div className="flex flex-col md:flex-row w-full gap-4">
+            <TextField
+              id="city"
+              label="City"
+              variant="standard"
+              className="md:w-1/2"
+            />
+            <TextField
+              id="country"
+              label="Country"
+              variant="standard"
+              className="md:w-1/2"
+            />
           </div>
           {/* Center the button */}
           <div className="text-center">
@@ -40,6 +74,6 @@ const Register: React.FC = () => {
       <Footer />
     </div>
   );
-}
+};
 
 export default Register;
