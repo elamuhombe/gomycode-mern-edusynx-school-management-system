875545d4115aa2ae8a162470e0524ba35a6f9c0e
Elaine Muhombe <emuhombe@gmail.com>
2024-03-30 18:57:31 +0300

    refractor: Modify Services route to Features

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
-import Services from './pages/Services';
+import Services from './pages/Features';
 import ContactUs from './pages/ContactUs';
 import Login from './pages/Login';
 import ResetPassword from './components/Login/ResetPassword';
 import Register from './pages/Register';
 import FAQ from './pages/support/FAQ';
 import Documentation from './pages/support/Documentation';
+import { GlobalStateProvider } from './context/useGlobalState';
 
 
 const App: React.FC = () => {
   return (
+    <GlobalStateProvider>
     <Router>
       <div className="m-4 font-sans">
       
@@ -21,7 +23,7 @@ const App: React.FC = () => {
           <Route path ="/reset-password" element={<ResetPassword/>} />
           <Route path="/Register" element={<Register />} />
           <Route path="/ContactUs" element={<ContactUs />} />
-          <Route path="/Services" element={<Services />} />
+          <Route path="/Features" element={<Services />} />
           <Route path="/support/FAQ" element ={<FAQ />} />
           <Route path="/support/Documentation" element ={<Documentation/>} />
           <Route path="/" element={<Home />} />
@@ -29,6 +31,7 @@ const App: React.FC = () => {
       
       </div>
     </Router>
+    </GlobalStateProvider>
   );
 };
 
