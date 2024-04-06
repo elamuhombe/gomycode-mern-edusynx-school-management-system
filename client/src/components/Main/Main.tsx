68bdcaabf44bffd0cc5ea4d31974fe28f45efc06
Elaine Muhombe <emuhombe@gmail.com>
2024-04-07 00:55:46 +0300

    feat: Add Contact form component

--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Hero from "./Hero";
 // import Features from "./Features";
-import Contacts from "./Contacts/Contacts";
+import Contacts from "../../components/Contacts/ContactForm"
 
 export default function Main() {
-
-
   return (
     <div className="flex flex-col px-6 min-h-[600px]">
-      
       <Hero />
       {/* <Features /> */}
       <Contacts />
