740bb00f881be95a2571b039099917661fbb9e60
Elaine Muhombe <emuhombe@gmail.com>
2024-04-07 00:49:12 +0300

    refractor: Add loggedInUser state

--- client/src/pages/dashboard/AdminDashboard.tsx
+++ client/src/pages/dashboard/AdminDashboard.tsx
@@ -3,17 +3,20 @@ import LeftMenu from "../../components/dashboard/shared/LeftMenu";
 import Topbar from "../../components/shared/Topbar";
 import Card from "../../components/dashboard/shared/cards/Card";
 import GenderBarChart from "../../components/dashboard/admin/charts/GenderBarChart";
+import { useGlobalState } from '../../hooks/useGlobalContext';
 
 
 
 const AdminDashboard: React.FC = () => {
-
+  const {state} = useGlobalState();
 
  
   return (
     <div className="bg-gray-200">
+        <h2>{state.loggedInUser?.name || 'no user'}</h2>
       <div className="top-0 z-0">
         <Topbar title="Admin Dashboard" />
+      
       </div>
       <div className="flex">
         <LeftMenu />
