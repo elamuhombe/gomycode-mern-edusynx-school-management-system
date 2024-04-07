2176ad84fa17cd6465c8257e64d65b712c14e71e
Elaine Muhombe <emuhombe@gmail.com>
2024-04-07 20:14:49 +0300

    feat: Add route for AddTeacher component

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,6 +2,7 @@ export { default as Home } from './Home';
 export { default as Features } from './Features';
 export { default as Contact } from './Contact';
 export { default as Login } from './Login';
+export { default as Register } from './Register';
 export { default as FAQ } from './support/FAQ';
 export { default as Documentation } from './support/Documentation';
 export { default as AdminDashboard } from './dashboard/AdminDashboard';
@@ -10,3 +11,4 @@ export { default as EnrollmentOfficerDashboard } from './dashboard/EnrollmentOff
 export { default as ParentDashboard } from './dashboard/ParentDashboard';
 export { default as AccountantDashboard } from './dashboard/Accountant';
 export { default as HeadTeacherDashboard } from './dashboard/HeadTeacherDashboard'
+export {default as AddTeacher} from './dashboard/AddTeacher';
