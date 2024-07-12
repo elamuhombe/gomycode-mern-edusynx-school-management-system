7e39c5565a1166baccefd4a028b9cd91c447a03b
Elaine Muhombe <emuhombe@gmail.com>
2024-07-12 09:46:17 +0300

    feat: Create a model for the invoice - Define Enum fr the Status - Define the interface for the invoice document - Define interface for the invoice model

---
+++ server/src/models/Invoice.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema, Document, ,Model, Types } from "mongoose";
+import { ISchool } from "./School";
+import { IStudent } from "./Student";
+
+// Enum for Status
+enum Status{
+    PENDING="pending",
+    OVERDUE="overdue",
+    COMPLETED="completed"
+}
+// interface for the invoice document
+interface IInvoice extends Document{
+    amount:Float32Array;
+    dueDate: Date;
+    status: Status;
+    studentId: string | IStudent
+}
+
+// inteface for the invoice model
+interface IInvoiceModel extends Model <IInvoice>{}
+
+
