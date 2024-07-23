fd6a1a3b60e3c8ded0d675e8b4f59f38d9ab3440
Elaine Muhombe <emuhombe@gmail.com>
2024-07-23 10:54:28 +0300

    feat: Create schema for the invoice

--- server/src/models/Invoice.ts
+++ server/src/models/Invoice.ts
@@ -1,22 +1,48 @@
 import mongoose, { Schema, Document, Model, Types } from "mongoose";
-import { ISchool } from "./School";
 import { IStudent } from "./Student";
 
 // Enum for Status
-enum Status{
-    PENDING="pending",
-    OVERDUE="overdue",
-    COMPLETED="completed"
+enum Status {
+    PENDING = "pending",
+    OVERDUE = "overdue",
+    COMPLETED = "completed"
 }
-// interface for the invoice document
-interface IInvoice extends Document{
-    amount:Float32Array;
+
+// Interface for the invoice document
+interface IInvoice extends Document {
+    amount: number;
     dueDate: Date;
     status: Status;
-    studentId: string | IStudent
+    studentId: Types.ObjectId | IStudent;
 }
 
-// inteface for the invoice model
-interface IInvoiceModel extends Model <IInvoice>{}
+// Interface for the invoice model
+interface IInvoiceModel extends Model<IInvoice> {}
 
+// Schema for the invoice
+const InvoiceSchema: Schema<IInvoice> = new Schema({
+    amount: {
+        type: Number,
+        required: true
+    },
+    dueDate: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: Status,
+        default: Status.PENDING
+    },
+    studentId: {
+        type: Schema.Types.ObjectId,
+        ref: "Student",
+        required: true
+    }
+}, {
+    timestamps: true
+});
 
+const Invoice: IInvoiceModel = mongoose.model<IInvoice, IInvoiceModel>("Invoice", InvoiceSchema);
+
+export { IInvoice, IInvoiceModel, Invoice };
